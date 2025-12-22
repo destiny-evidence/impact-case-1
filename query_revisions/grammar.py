985d5c4f5931216f43dc647b523ecfeee0be08fe
Tim Repke <repke@mcc-berlin.net>
2025-12-22 21:38:29 +0100

    add other transformer

--- query_revisions/grammar.py
+++ query_revisions/grammar.py
@@ -1,22 +1,22 @@
 import re
 
-from lark import Lark, Tree, Token
+from lark import Lark, Tree, Token, Transformer
 
 grammar = Lark(
     '''
-    ?start: or
+    ?start: op_or
     
-    ?or: and 
-        | or "OR" and           -> or
-        | or near and           -> near
+    ?op_or: op_and 
+        | op_or "OR" op_and           -> op_or
+        | op_or near op_and           -> near
     
     ?near: "NEAR/" NUMBER
          | "W/" NUMBER
     
-    ?and: atom
-        | and "AND NOT" atom    -> not
-        | and "NOT" atom        -> not
-        | and "AND" atom        -> and
+    ?op_and: atom
+        | op_and "AND NOT" atom    -> op_not
+        | op_and "NOT" atom        -> op_not
+        | op_and "AND" atom        -> op_and
     
     ?phrase_inner: token
          | token phrase_inner
@@ -33,7 +33,7 @@ grammar = Lark(
     
     ?atom: token
          | phrase
-         | "(" or ")"
+         | "(" op_or ")"
     
     _QUOT : "\\""
     
@@ -60,12 +60,74 @@ grammar = Lark(
 NEAR_OFFSET = 0
 
 
+class Node:
+    def __init__(self, label, children=None):
+        self.label = label
+        self.children = children or []
+
+    def render(self, indent=0):
+        pad = '  ' * indent
+        if not self.children:
+            return self.label
+        lines = [child.render(indent + 1) for child in self.children]
+
+        return f'{pad}(\n'+(f'\n{pad} {self.label} '.join(lines))+f'\n{pad})'
+
+
 def expand_wildcard(token: str, prefix: str, postfix: str, expansions: dict[str, list[str]]):
     if token in expansions:
         return f'({' OR '.join(expansions[token])})'
     return f'{prefix}{token}{postfix}'
 
-def parse(query, expansions: dict[str, list[str]] | None = None):
+
+class QueryTransformer(Transformer):
+    def __init__(self, visit_tokens: bool = True, expansions: dict[str, list[str]] | None = None):
+        super().__init__(visit_tokens=visit_tokens)
+        self.expansions = expansions or {}
+
+    def WORD(self, t):
+        return Node(str(t))
+
+    def NUMBER(self, t):
+        return Node(str(t))
+
+    def phrase_inner(self, items):
+        return Node('(' + (' W '.join(i.label for i in items)) + ')')
+
+    def wild_pre(self, items):
+        return Node(expand_wildcard(items[0].label, prefix='', postfix='*', expansions=self.expansions))
+
+    def wild_post(self, items):
+        # FIXME: surround QueryParser doesn't like wildcards as prefix
+        # return Node(self.expand_wildcard(items[0].label, prefix='*', postfix=''))
+        return Node(expand_wildcard(items[0].label, prefix='', postfix='', expansions=self.expansions))
+
+    def wild_prepost(self, items):
+        # FIXME: surround QueryParser doesn't like wildcards as prefix
+        # return Node(self.expand_wildcard(items[0].label, prefix='*', postfix='*'))
+        return Node(expand_wildcard(items[0].label, prefix='', postfix='*', expansions=self.expansions))
+
+    def wild_one(self, items):
+        return Node(f'{items[0].label}?')
+
+    def wild_in(self, items):
+        return Node(f'{items[0].label}?{items[1].label}')
+
+    def op_and(self, items):
+        return Node('AND', items)
+
+    def op_or(self, items):
+        return Node('OR', items)
+
+    def op_not(self, items):
+        return Node('NOT', items)
+
+    def near(self, items):
+        distance = int(items[1].label) + NEAR_OFFSET
+        return Node('W' if distance < 2 else f'{distance}W', items[:1] + items[2:])
+
+
+def parse(query, expansions: dict[str, list[str]] | None = None, parent: str | None = None):
     comm = re.compile(r'# .*\n')
     tree = grammar.parse(comm.sub('', query))
     expansions = {} if expansions is None else expansions
@@ -77,7 +139,7 @@ def parse(query, expansions: dict[str, list[str]] | None = None):
         if not isinstance(subtree, Tree):
             raise SyntaxError('This is not a tree!')
 
-        if subtree.data == 'or':
+        if subtree.data == 'op_or':
             return (
                 '(\n'
                 f'{indent}   {recurse(subtree.children[0], indent=indent + '  ')}\n'
@@ -85,7 +147,7 @@ def parse(query, expansions: dict[str, list[str]] | None = None):
                 f'{indent[:-2]})'
             )
 
-        if subtree.data == 'and':
+        if subtree.data == 'op_and':
             return (
                 '(\n'
                 f'{indent}    {recurse(subtree.children[0], indent=indent + '  ')}\n'
@@ -93,7 +155,7 @@ def parse(query, expansions: dict[str, list[str]] | None = None):
                 f'{indent[:-2]})'
             )
 
-        if subtree.data == 'not':
+        if subtree.data == 'op_not':
             return (
                 '(\n'
                 f'{indent}    {recurse(subtree.children[0], indent=indent + '  ')}\n'
@@ -118,7 +180,7 @@ def parse(query, expansions: dict[str, list[str]] | None = None):
         if subtree.data == 'wild_pre':
             return expand_wildcard(subtree.children[0].value, prefix='', postfix='*', expansions=expansions)
         if subtree.data == 'wild_post':
-            return expand_wildcard(subtree.children[0].value, prefix='', postfix='', expansions=expansions) # FIXME: postfix not available in surround
+            return expand_wildcard(subtree.children[0].value, prefix='', postfix='', expansions=expansions)  # FIXME: postfix not available in surround
         if subtree.data == 'wild_prepost':
             # FIXME: postfix not available in surround
             # return ('('
@@ -134,3 +196,35 @@ def parse(query, expansions: dict[str, list[str]] | None = None):
         raise SyntaxError("You shouldn't end up here.")
 
     return recurse(tree)
+
+
+if __name__ == '__main__':
+    q = '''
+    (
+        (
+            heat 
+            W/2 (
+                   stress
+                OR fatigue
+                OR burn*
+                OR stroke
+                OR exhaustion
+                OR cramp*
+            )
+        )
+        OR skin
+        OR fever*
+        OR rash*
+        OR eczema*
+        OR "thermal stress*"
+        OR hypertherm*
+        OR hypotherm*
+        OR "thermal stability"
+    )'''
+    # print(parse(q))
+    print('--------------')
+    prepared = re.compile(r'# .*\n').sub('', q)
+    tree = grammar.parse(prepared)
+    print(tree)
+    processed = QueryTransformer(expansions={'burn': ['burn', 'burned', 'burns']}).transform(tree)
+    print(processed.render())
