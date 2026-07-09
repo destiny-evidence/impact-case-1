52f98beb758fabd4c1e92991c977a37a6565181d
mcallaghan <max.callaghan@pik-potsdam.de>
2026-07-09 16:57:48 +0200

    refactor export annotations, put more definitions and paths in config, using a taskconfig dataclass

--- ic1/core/config.py
+++ ic1/core/config.py
@@ -1,6 +1,8 @@
 """config for IC1 scripts."""
 from typing import TypedDict
 from pathlib import Path
+from enum import Enum
+from dataclasses import dataclass
 
 from ic1.core.ids import (
     INOUT_SCHEME_ID,
@@ -9,14 +11,41 @@ from ic1.core.ids import (
     TAXONOMY_SCOPE_IDS
 )
 
-class TaskConfig(TypedDict):
+class TaskName(str, Enum):
+    INOUT = 'inout'
+    TAXONOMY = 'taxonomy'
+    ALL = 'all'
+
+@dataclass
+class TaskConfig:
+    name: str
     scheme_id: str
     scope_ids: list[str]
 
+    @property
+    def shareable_path(self) -> Path:
+        return SHAREABLE_ROOT / f'{self.name}.csv'
+
+    @property
+    def sensitive_path(self) -> Path:
+        return SENSITIVE_ROOT / f'{self.name}.csv'
+
+    @property
+    def resolved_path(self) -> Path:
+        return SENSITIVE_ROOT / f'{self.name}_resolved.csv'
+
 
 TASKS: dict[str, TaskConfig] = {
-    'inout': {'scheme_id': INOUT_SCHEME_ID, 'scope_ids': INOUT_SCOPE_IDS},
-    'taxonomy': {'scheme_id': TAXONOMY_SCHEME_ID, 'scope_ids': TAXONOMY_SCOPE_IDS},
+    'inout': TaskConfig(
+        name= "inout",
+        scheme_id = INOUT_SCHEME_ID,
+        scope_ids = INOUT_SCOPE_IDS
+    ),
+    'taxonomy': TaskConfig(
+        name="taxonomy",
+        scheme_id=TAXONOMY_SCHEME_ID,
+        scope_ids=TAXONOMY_SCOPE_IDS
+    )
 }
 
 CONF_FILE = '.conf/secret.env'
